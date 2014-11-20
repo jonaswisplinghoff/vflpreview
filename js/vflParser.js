4573195047316c169ac906e9a8098729c8ca9bc5
Jonas Wisplinghoff <mail@jonaswisplinghoff.de>
2014-11-20 19:26:31 +0100

    added some stylesheet basics

--- js/vflParser.js
+++ js/vflParser.js
@@ -3,11 +3,12 @@ $( document ).ready(function() {
     var vflString = $("#vflString").val();
     console.log(vflString);
 
-    var regex = /([HV]:)?(\|(-\d*-|-)?)?(\[[a-zA-Z0-9]+(\((==|<=|>=)?(\d*|[a-zA-Z0-9]+)(@\d*)?(,(==|<=|>=)?(\d*|[a-zA-Z0-9]+)(@\d*)?)*\))?\])((-\d*-|-)?(\[[a-zA-Z0-9]+(\((==|<=|>=)?(\d*|[a-zA-Z0-9]+)(@\d*)?(,(==|<=|>=)?(\d*|[a-zA-Z0-9]+)(@\d*)?)*\))?\]))*((-\d*-|-)?\|)?/
+    var viewRegex = /(\[[a-zA-Z0-9]+(\((==|<=|>=)?(\d*|[a-zA-Z0-9]+)(@\d*)?(,(==|<=|>=)?(\d*|[a-zA-Z0-9]+)(@\d*)?)*\))?\])/
+    var completeRegex = /([HV]:)?(\|(-\d*-|-)?)?(\[[a-zA-Z0-9]+(\((==|<=|>=)?(\d*|[a-zA-Z0-9]+)(@\d*)?(,(==|<=|>=)?(\d*|[a-zA-Z0-9]+)(@\d*)?)*\))?\])((-\d*-|-)?(\[[a-zA-Z0-9]+(\((==|<=|>=)?(\d*|[a-zA-Z0-9]+)(@\d*)?(,(==|<=|>=)?(\d*|[a-zA-Z0-9]+)(@\d*)?)*\))?\]))*((-\d*-|-)?\|)?/
 
-    if(regex.test(vflString)){
+    if(completeRegex.test(vflString)){
       console.log("Erkannt!");
-      var result = regex.exec(vflString);
+      var result = completeRegex.exec(vflString);
       for(var i=0; i<result.length; i++){
         console.log(i + ": " + result[i]);
       }
