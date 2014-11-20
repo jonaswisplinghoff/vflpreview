15510de49a907a40c1e926779c46633b24361693
Jonas Wisplinghoff <mail@jonaswisplinghoff.de>
2014-11-20 20:16:16 +0100

    some refactorings

--- js/vflParser.js
+++ js/vflParser.js
@@ -4,25 +4,34 @@
 // regex: ([HV]:)?(\|(-\d*-|-)?)?(\[[a-zA-Z0-9]+(\((==|<=|>=)?(\d*|[a-zA-Z0-9]+)(@\d*)?(,(==|<=|>=)?(\d*|[a-zA-Z0-9]+)(@\d*)?)*\))?\])((-\d*-|-)?(\[[a-zA-Z0-9]+(\((==|<=|>=)?(\d*|[a-zA-Z0-9]+)(@\d*)?(,(==|<=|>=)?(\d*|[a-zA-Z0-9]+)(@\d*)?)*\))?\]))*((-\d*-|-)?\|)?
 // http://regexr.com/39uil
 
+var viewRegex = /(\[[a-zA-Z0-9]+(\((==|<=|>=)?(\d*|[a-zA-Z0-9]+)(@\d*)?(,(==|<=|>=)?(\d*|[a-zA-Z0-9]+)(@\d*)?)*\))?\])/;
+var completeRegex = /([HV]:)?(\|(-\d*-|-)?)?(\[[a-zA-Z0-9]+(\((==|<=|>=)?(\d*|[a-zA-Z0-9]+)(@\d*)?(,(==|<=|>=)?(\d*|[a-zA-Z0-9]+)(@\d*)?)*\))?\])((-\d*-|-)?(\[[a-zA-Z0-9]+(\((==|<=|>=)?(\d*|[a-zA-Z0-9]+)(@\d*)?(,(==|<=|>=)?(\d*|[a-zA-Z0-9]+)(@\d*)?)*\))?\]))*((-\d*-|-)?\|)?/;
+
 $( document ).ready(function() {
   $("#vflString").change(function(){
     var vflString = $("#vflString").val();
-    console.log(vflString);
+    console.log("Textfield changed to: " + vflString);
 
+    // Just testing
     $("#content").text(vflString);
 
-    var viewRegex = /(\[[a-zA-Z0-9]+(\((==|<=|>=)?(\d*|[a-zA-Z0-9]+)(@\d*)?(,(==|<=|>=)?(\d*|[a-zA-Z0-9]+)(@\d*)?)*\))?\])/;
-    var completeRegex = /([HV]:)?(\|(-\d*-|-)?)?(\[[a-zA-Z0-9]+(\((==|<=|>=)?(\d*|[a-zA-Z0-9]+)(@\d*)?(,(==|<=|>=)?(\d*|[a-zA-Z0-9]+)(@\d*)?)*\))?\])((-\d*-|-)?(\[[a-zA-Z0-9]+(\((==|<=|>=)?(\d*|[a-zA-Z0-9]+)(@\d*)?(,(==|<=|>=)?(\d*|[a-zA-Z0-9]+)(@\d*)?)*\))?\]))*((-\d*-|-)?\|)?/;
-
     if(completeRegex.test(vflString)){
-      console.log("Erkannt!");
-      var result = completeRegex.exec(vflString);
-      for(var i=0; i<result.length; i++){
-        console.log(i + ": " + result[i]);
-      }
+      validVflString(vflString)
     }
     else {
-      console.log("Nicht erkannt!");
+      invalidVflString()
     }
   });
 });
+
+var validVflString = function(vflString){
+  console.log("Valid VFL!");
+  var result = completeRegex.exec(vflString);
+  for(var i=0; i<result.length; i++){
+    console.log(i + ": " + result[i]);
+  }
+};
+
+var invalidVflString = function(){
+  console.log("Invalid VFL!");
+};
