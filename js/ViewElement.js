2de555e79c7361febf11878879832a1fc70a5a03
David <david.rink14@googlemail.com>
2015-01-21 20:09:10 +0100

    Revert "bisschen im ViewElement rumgepfuscht (weiß noch nicht wie stabil das ist, aber muss jetzt ins training^^) und n paar Logs auskommentiert."
    This reverts commit cfa9d29b9fed6feaa966ae0eec4a069f6498f016.

--- js/ViewElement.js
+++ js/ViewElement.js
@@ -3,14 +3,7 @@ function ViewElement(id, contentElement) {
   var LOG = "ViewElement: ";
   var domElement;
   var domElementId;
-  var contentElement;
-
-  var width=100;
-  var height=50;
-  var marginTop=0;
-  var marginRight=0;
-  var marginBottom=0;
-  var marginLeft=0;
+  var contentElement
 
   var construct = function(id, contentEl) {
     domElementId=id;
@@ -22,36 +15,26 @@ function ViewElement(id, contentElement) {
       contentElement.append("<div class='view' id='"+domElementId+"'>"+domElementId+"</div>");
       domElement=$('#'+domElementId);
       //console.log('drawing: '+domElementId);
-      refreshProperties();
     }
   };
 
-  this.setWidth = function(w) {
-    if (w != "undefined") {
-      width=parseInt(w);
-      refreshProperties();
-    }
-  };
-
-  this.setHeight = function(h) {
-    if (h != "undefined") {
-      height=parseInt(h);
-      refreshProperties();
-    }
-  };
-
-  var refreshProperties = function() {
-    console.log("id: "+domElementId+" - "+width+"; "+height+"; "+marginTop+"; "+marginRight+"; "+marginBottom+"; "+marginLeft);
-
-    //if($("#"+domElementId).length === 0){
-    if(domElement.length === 0){
+  this.setWidth = function(width) {
+    //TODO: implement more sophisticated layout logic
+    if($("#"+domElementId).length === 0){
       console.log("domElement of "+domElementId+" does not yet exist.");
     } else {
-      domElement.css("width", width);
-      domElement.css("height", height);
-      domElement.css("margin", marginTop+"px "+marginRight+"px "+marginBottom+"px "+marginLeft+"px");
+      $("#"+domElementId).css("width", width);
     }
-  }
+  };
+
+  this.setHeight = function(height) {
+    //TODO: implement more sophisticated layout logic
+    if($("#"+domElementId).length === 0){
+      console.log("domElement of "+domElementId+" does not yet exist.");
+    } else {
+      $("#"+domElementId).css("height", height);
+    }
+  };
 
   construct(id, contentElement);
 }
